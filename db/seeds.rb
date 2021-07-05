ca4181ac0c3bd597c7ee3835221d8016c3c1c554
yuukii125 <y.kipro125@gmail.com>
2021-07-05 22:52:50 +0900

    seedデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post_params = [
+  {
+    title: "React",
+    content: "ユーザインターフェース構築のための JavaScript ライブラリ"
+  },
+  {
+    title: "Vue.js",
+    content: "The Progressive JavaScript Framework"
+  },
+  {
+    title: "Angular",
+    content: "モバイルとデスクトップ、ひとつのフレームワーク"
+  }
+]
+
+Post.delete_all
+Post.create!(post_params)
+puts "初期データの投入に成功しました!"
